4854454441597d0e0de8c77f8013e042d4f662b4
Nelson An <hoangquangan63@gmail.com>
2024-05-02 11:29:14 +0700

    primary learning

---
+++ primary/src/main/java/com/example/primary/common/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.example.primary.common;
+
+public class BaseballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice";
+    }
+}
